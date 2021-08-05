9ca495de79957cea9b1adc090afb256dbafc3a86
William Niemiec <williamniemiec@hotmail.com>
2021-08-05 18:58:55 -0300

    Update Encryptors.java

--- src/main/java/wniemiec/util/data/Encryptors.java
+++ src/main/java/wniemiec/util/data/Encryptors.java
@@ -1,5 +1,15 @@
+/**
+ * Copyright (c) William Niemiec.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package wniemiec.util.data;
 
+/**
+ * Responsible for obtaining instances of encrypters.
+ */
 public class Encryptors {
 
     private Encryptors() {
